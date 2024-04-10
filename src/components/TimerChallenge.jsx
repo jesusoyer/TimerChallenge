56e8eddf2d52054a5b288a83da1530438389b217
jesusoyer <oyervidesjesus017@gmail.com>
2024-04-10 16:02:40 -0500

    created a modal component to open when timer times out using the forwardref hook

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,19 +1,43 @@
+import { useState, useRef } from 'react';
+import ResultModal from './ResultModal';
 
 
 const TimerChallenge = ({title, targetTime}) => {
+const timer = useRef();
+const dialog = useRef();
+
+const [timerStarted, setTimerStarted] = useState(false);    
+const [timerExpired, setTimeExpired] = useState(false);
+
+
+
+function handleStart (){
+    timer.current = setTimeout(()=> {setTimeExpired(true); dialog.current.showModal()},
+     targetTime * 1000 )
+    setTimerStarted(true)
+}
+function handleStop(){
+    clearTimeout(timer.current);
+}
+
+
   return (
+    <>
+    <ResultModal ref={dialog} targetTime={targetTime} result='lost'/>
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You Lost!</p>}
       <p className="challenge-time">
         {targetTime} second {targetTime > 1 ? 's' : ''}
       </p>
       <p>
-      <button>Start Challenge</button>
+      <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
       </p>
-      <p className="">
-        Time is running... /timer inactive
+      <p className={timerStarted ? 'active': undefined}>
+        {timerStarted ? "Time is running..." : "timer inactive"}
       </p>
     </section>
+    </>
   )
 }
 
